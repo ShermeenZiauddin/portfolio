f173c5950165b265ff52955b5c979ee2c5e56cb3
ShermeenZiauddin <shermeen289@gmail.com>
2025-07-11 11:10:17 +0500

    first commit

---
+++ scrolltop.js
@@ -0,0 +1,11 @@
+const toTop = document.querySelector('.toTop');
+window.addEventListener('scroll',()=>{
+    if(window.scrollY > 40){
+        toTop.style.opacity = '1';
+    } else{
+        toTop.style.opacity='0'
+    }
+})
+toTop.addEventListener('click',()=>{
+    window.scrollTo(0,0)
+})
